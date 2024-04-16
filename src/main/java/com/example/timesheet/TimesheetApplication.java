40039f28a0cc6a6b4291d38fada288b257a374b1
mokhtar194 <74710785+mokhtar194@users.noreply.github.com>
2024-04-16 14:20:45 +0200

    Update TimesheetApplication.java

--- src/main/java/com/example/timesheet/TimesheetApplication.java
+++ src/main/java/com/example/timesheet/TimesheetApplication.java
@@ -27,10 +27,10 @@ public class TimesheetApplication {
     CommandLineRunner commandLineRunnerUserDetails(UserService userService){
         return args -> {
            // User addNewUser(String nom, String prenom,  String email,String password, String confirmPassword);
-             userService.addNewRole("COllAB");
-             userService.addNewRole("ADMIN");
-             userService.addNewUser("admin", "admin","admin@gmail.com","1234", "1234");
-           userService.addRoleToUser("admin@gmail.com","ADMIN");
+           //  userService.addNewRole("COllAB");
+           //  userService.addNewRole("ADMIN");
+            // userService.addNewUser("admin", "admin","admin@gmail.com","1234", "1234");
+           // userService.addRoleToUser("admin@gmail.com","ADMIN");
          //   userService.addNewUser("aziz1", "azizaziz1","aziz1@gmail.com","1234", "1234");
            // userService.addRoleToUser("aziz1@gmail.com","COllAB");
             // userService.addNewUser("user2","1234","user1@gmail.com","1234");
